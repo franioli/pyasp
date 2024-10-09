1cc03c8b952558f16a05c137680e1343054e3990
Francesco Ioli <francesco.ioli@geo.uzh.ch>
2024-10-09 11:45:38 +0200

    updated init

--- pyASP/__init__.py
+++ pyASP/__init__.py
@@ -1,2 +1,7 @@
 from .utils.logger import change_logger_level, get_logger, setup_logger  # noqa: F401
 from .utils.timer import Timer, timeit  # noqa: F401
+
+__version__ = "1.0.0"
+
+logger = setup_logger(name="pyasp", log_level="info")
+timer = Timer(logger=logger)
